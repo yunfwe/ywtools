993dd1dd932d0a21d507305d26ee85e70de4484f
yunfwe <1441923087@qq.com>
2017-08-31 19:54:30 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ywtools',
+    version='1.0.0',
+    packages=['app', 'app.core', 'app.test', 'app.tools', 'app.utils', 'app.views', 'app.views.routers', 'app.scripts',
+              'app.database'],
+    url='',
+    license='GPL',
+    author='yunfwe',
+    author_email='1441923087@qq.com',
+    description=''
+)
