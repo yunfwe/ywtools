cac49a1a7819fd79daf5e7adfc8b4d42ef81af5b
yunfwe <1441923087@qq.com>
2017-08-31 19:42:26 +0800

    json

---
+++ app/test/daemon.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author: weiyunfei  date: 2017-08-31
+
+import os,pwd
+import time
+from sys import argv
+
+pid = os.fork()
+if pid:exit()
+subpid = os.getpid()
+os.setuid(99)
+os.setsid()
+open('/tmp/pid.log','a').write('[python]: subprocess run on '+str(subpid))
+time.sleep(100000)
---
+++ app/test/forktest.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author: weiyunfei  date: 2017-08-31
+
+import os
+import time
+import sys,pwd
+
+child_process = []
+
+print('before fork')
+for i in range(10):
+    pid = os.fork()
+    print('after fork')
+    ppid = os.getppid()
+
+    if pid == 0:
+        os.setuid(99)
+        os.setsid()
+        print('This is child process pid:%s,ppid:%s' % (os.getpid(),ppid))
+        time.sleep(100)
+    else:
+        print('This is parent process pid:%s' % pid)
+os.wait()
+
