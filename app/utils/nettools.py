508fb87a4698f0135f4d237fbf1671cbec06a3e0
yunfwe <1441923087@qq.com>
2017-09-09 22:35:24 +0800

    setup

--- app/utils/nettools.py
+++ app/utils/nettools.py
@@ -2,7 +2,61 @@
 # -*- coding:utf-8 -*-
 # author: weiyunfei  date: 2017-09-09
 
-import socket
+from __future__ import absolute_import
+from __future__ import print_function
 
-def getaddrinfo(host):
-    return tuple(set(map(lambda x:x[4][0],socket.getaddrinfo(host,None))))
+import os
+import socket
+import fcntl
+import struct
+
+
+# 分析/proc/net/dev文件 获取主机网卡接口名称
+def getInterface():
+    # with open('/proc/net/dev') as f:
+    #     raw = f.readlines()
+    #     return tuple(map(lambda x:x.split(':')[0].strip(),raw[2:]))
+    return tuple(os.listdir('/sys/class/net/'))
+
+# 获取网卡的mac地址
+def getMacByInterface(interface):
+    try:
+        return open('/sys/class/net/{inface}/address'
+                    .format(inface=interface)).read().strip()
+    except:
+        return False
+
+# 获取网卡绑定的IP地址
+def getAddrByInterface(interface):
+    interface = interface.encode()
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        return socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', interface[:15])
+        )[20:24])
+    except:
+        return False
+
+# 通过DNS查询主机的IP地址
+def getAddrInfo(host):
+    try:
+        return tuple(set(map(lambda x:x[4][0], socket.getaddrinfo(host,None))))
+    except:
+        return False
+
+# DNS反向查询IP
+def getHostByAddr(ip):
+    try:
+        return socket.gethostbyaddr(ip)[0]
+    except:
+        return False
+
+# 向目标主机发送ICMP包
+def sendIcmpPackage(ip):
+    pass
+
+if __name__ == '__main__':
+    for i in getInterface():
+        print(i+(11-len(i))*' '+'\t'+getMacByInterface(i)+'\t\t'+str(getAddrByInterface(i)))
