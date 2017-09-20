c5b036e1f7d6a65a530fcd888a06e31ce24b4fab
yunfwe <1441923087@qq.com>
2017-09-20 23:35:23 +0800

    remotescript

--- app/utils/remotescript0.1.py
+++ app/utils/remotescript0.1.py
@@ -99,13 +99,13 @@ class Config(object):
         'ssh_port': 22,
         'ssh_username': 'root',
         'ssh_password': None,
-        'ssh_sudo_password':None,
+        'ssh_su_password':None,
         'scripts_dir': '/usr/share/remotescripts/',
         'config_file': '/etc/remotescript.conf',
         'ssh_connect_timeout': 10,
         'ssh_auth_timeout': 10,
         'ssh_execute_timeout': 60,
-        'log_file': '/var/log/remotescript.log',
+        'log_file': '/tmp/remotescript.log',
         'script': None,
         'append':None,
         'debug': False,
@@ -182,7 +182,7 @@ class Config(object):
 def init():
     template = ('[general]\n'
                 '# default_scripts_dir = /usr/share/remotescripts/\n'
-                '# log_file = /var/log/remotescript.log\n'
+                '# log_file = /tmp/remotescript.log\n'
                 '\n'
                 '[ssh]\n'
                 '# default_ssh_host = 127.0.0.1\n'
@@ -234,7 +234,6 @@ class SSH(object):
         self.sftp = self._ssh.open_sftp()
     def _execute(self, command, timeout=None):
         return self._ssh.exec_command("bash -c \"{cmd}\"".format(cmd=command), get_pty=True, timeout=timeout)
-
     def execute(self, command, timeout=None):
         rst_stdin, rst_stdout, rst_stderr =  self._ssh.exec_command("bash -c \"{cmd}\"".format(cmd=command), get_pty=True,
                                       timeout=timeout)
@@ -256,36 +255,57 @@ class SSH(object):
         os.chmod(dest,mode)
     def close(self):self._ssh.close()
 
-def script():pass
-def cmd():
-    cf = Config.config
+
+def cmd(ssh,cf):
     if cf['append'] is None:
         sys.stderr.write('You must privode -a or --append for cmd action! Use --help see more.\n')
+        sys.exit(1)
     if not cf['sep'] is None:
         cf['append'] = ' '.join(cf['append'].split(cf['sep']))
 
-    ssh = SSH(hostname=cf['ssh_host'], username=cf['ssh_username'], password=cf['ssh_password'],
-              timeout=cf['ssh_connect_timeout'],auth_timeout=cf['ssh_auth_timeout'])
-    if cf.get('sudo'):
-        if not cf['ssh_sudo_password']:
-            cf['ssh_sudo_password'] = __import__('getpass').getpass("Please input sudo password: ")
-        if cf['append'][:5] == 'sudo ':
-            rst = ssh._execute(cf['append'], timeout=cf['ssh_execute_timeout'])
-        else:
-            rst = ssh._execute('sudo '+cf['append'], timeout=cf['ssh_execute_timeout'])
-        rst[0].write(cf['ssh_sudo_password']+'\n')
+    if cf.get('su'):
+        if not cf['ssh_su_password']:
+            cf['ssh_su_password'] = __import__('getpass').getpass("Remote %s's password: " % cf['su'])
+        rst = ssh._execute('su - {user} -c "'.format(user=cf['su'])+cf['append']+'"', timeout=cf['ssh_execute_timeout'])
         try:
-            err = ''.join(rst[2].readlines()[2:])
+            if rst[1].read(1):
+                rst[0].write(cf['ssh_su_password']+'\n')
+            err = ''.join(rst[2].readlines()[1:])
             if err:
+                print(err)
                 sys.stderr.write(err)
-            result = ''.join(rst[1].readlines()[2:])
+                sys.stderr.flush()
+            result = ''.join(rst[1].readlines()[1:])
             if result:
                 sys.stdout.write(result)
         except timeout:
             sys.stdout.write('Warnning: Host {ip}: execute "{cmd}" timeout!\n'.format(ip=ssh.info[0],cmd=cf['append']))
             log.warning('Host {ip}: execute "{cmd}" timeout!\n'.format(ip=ssh.info[0],cmd=cf['append']))
             sys.exit(2)
-        sys.exit(rst[1].channel.recv_exit_status())
+        log.info('Host {ip}: execute "{cmd}" successful!'.format(ip=ssh.info[0],cmd=cf['append']))
+        # sys.exit(rst[1].channel.recv_exit_status())
+        return rst[1].channel.recv_exit_status()
+    elif cf.get('sudo'):
+        if cf['append'][:5] == 'sudo ':
+            rst = ssh._execute(cf['append'], timeout=cf['ssh_execute_timeout'])
+        else:
+            rst = ssh._execute('sudo -i '+cf['append'], timeout=cf['ssh_execute_timeout'])
+        try:
+            if rst[1].read(1):
+                rst[0].write(cf['ssh_password']+'\n')
+            err = ''.join(rst[2].readlines()[1:])
+            if err:
+                sys.stderr.write(err)
+            result = ''.join(rst[1].readlines()[1:])
+            if result:
+                sys.stdout.write(result)
+        except timeout:
+            sys.stdout.write('Warnning: Host {ip}: execute "{cmd}" timeout!\n'.format(ip=ssh.info[0],cmd=cf['append']))
+            log.warning('Host {ip}: execute "{cmd}" timeout!\n'.format(ip=ssh.info[0],cmd=cf['append']))
+            sys.exit(2)
+        log.info('Host {ip}: execute "{cmd}" successful!'.format(ip=ssh.info[0],cmd=cf['append']))
+        # sys.exit(rst[1].channel.recv_exit_status())
+        return rst[1].channel.recv_exit_status()
     else:
         try:
             rst = ssh.execute(cf['append'],timeout=cf['ssh_execute_timeout'])
@@ -293,22 +313,133 @@ def cmd():
             sys.stdout.write('Warnning: Host {ip}: execute "{cmd}" timeout!\n'.format(ip=ssh.info[0],cmd=cf['append']))
             log.warning('Host {ip}: execute "{cmd}" timeout!\n'.format(ip=ssh.info[0],cmd=cf['append']))
             sys.exit(2)
-        log.info('Host {ip}: execute "{cmd}" successful!'.format(ip=ssh.info[1],cmd=cf['append']))
         if rst[1]:sys.stderr.write(rst[1])
         if rst[0]:sys.stdout.write(rst[0])
-        sys.exit(rst[2])
+        log.info('Host {ip}: execute "{cmd}" successful!'.format(ip=ssh.info[0],cmd=cf['append']))
+        # sys.exit(rst[2])
+        return rst[2]
 
 
-def put():pass
-def get():pass
+def put(ssh,cf):
+    try:
+        src = cf['src']
+        dest = cf['dest']
+    except KeyError:
+        print('You must privode --src and --dest for put or get action! Use --help see more.')
+        sys.exit(1)
+    if not os.path.exists(src):
+        sys.stdout.write('Error: No such file or directory: \'%s\'\n'%src)
+        sys.exit(1)
+    if not os.path.isfile(src):
+        sys.stdout.write('Error: Don\'t support directory!\n')
+        sys.exit(1)
+    if dest[-1] == '/':
+        dest = os.path.join(dest,os.path.split(src)[-1])
+    try:
+        ssh.sftp.stat(dest)
+    except FileNotFoundError:pass
+    else:
+        print('Error: Remote file already exists! \'%s\''%dest)
+        sys.exit(1)
+    try:
+        ssh.putfile(src=src,dest=dest)
+    except PermissionError as e:
+        sys.stdout.write('Error: %s!'%str(e))
+        sys.exit(1)
+    except FileNotFoundError:
+        sys.stdout.write('Error: The destination path does not exist! %s\n' % dest)
+        sys.exit(1)
+    except Exception as e:
+        sys.stdout.write('Error: %s\n' % str(e))
+        sys.exit(1)
+    # print("[Put]  src: %s  dest: %s successful" % (src,dest))
+    log.info("Host: %s [Put]  src: %s  dest: %s successful" % (ssh.info[0],src,dest))
+    # sys.exit(0)
+    return src,dest
+
+def get(ssh,cf):
+    try:
+        src = cf['src']
+        dest = cf['dest']
+    except KeyError:
+        print('You must privode --src and --dest for put or get action! Use --help see more.')
+        sys.exit(1)
+
+    if dest[-1] == '/':
+        if not os.path.exists(dest):
+            print('Error: Directory not exists! \'%s\''%dest)
+            sys.exit(1)
+        dest = os.path.join(dest,os.path.split(src)[-1])
+    if os.path.exists(dest):
+        print('Error: File already exists! \'%s\''%dest)
+        sys.exit(1)
+    try:
+        ssh.sftp.stat(src)
+    except FileNotFoundError:
+        print('Error: Remote file not exists! \'%s\''%src)
+        sys.exit(1)
+    try:
+        ssh.getfile(src=src,dest=dest)
+    except PermissionError as e:
+        sys.stdout.write('Error: %s!'%str(e))
+        sys.exit(1)
+    except Exception as e:
+        sys.stdout.write('Error: %s\n' % str(e))
+        sys.exit(1)
+    # print("[Get]  src: %s  dest: %s successful" % (src,dest))
+    log.info("Host: %s [Get]  src: %s  dest: %s successful" % (ssh.info[0],src,dest))
+    # sys.exit(0)
+    return src,dest
+
+
+def script(ssh,cf):
+    if not cf['script']:
+        sys.stderr.write("You must privode --script for script action! Use --help see more.\n")
+        sys.stderr.flush()
+        sys.exit(1)
+    if ssh.info[2] == 'root':
+        homePath = '/root'
+    else:
+        homePath = os.path.join('/home', ssh.info[2])
+    if cf['script'][-1] == '/':
+        sys.stderr.write("Error: --script option, You should privode file path instead of directory path.\n")
+        sys.stderr.flush()
+        sys.exit(2)
+    if '/' in cf['script']:
+        srcPath = cf['script']
+        dstfile = os.path.split(srcPath)[-1]
+    else:
+        if os.path.exists('./%s' % cf['script']):
+            srcPath = './'+cf['script']
+            dstfile = os.path.split(srcPath)[-1]
+        else:
+            srcPath = os.path.join(cf['scripts_dir'], cf['script'])
+            dstfile = cf['script']
+    destPath = os.path.join(homePath, '.remotescripts/', dstfile+Utils.random())
+    try:
+        ssh.sftp.stat(os.path.dirname(destPath))
+    except FileNotFoundError:
+        try:
+            ssh.sftp.mkdir(os.path.dirname(destPath))
+        except PermissionError as e:
+            sys.stderr('Host: %s %s' % (ssh.info[0],str(e)))
+            sys.exit(2)
+    cf['src'],cf['dest'] = srcPath,destPath
+    if cf['append'] is None:
+        cf['append'] = destPath
+    else:
+        if not cf['sep'] is None:
+            cf['append'] = destPath+' '+' '.join(cf['append'].split(cf['sep']))
+    put(ssh,cf)
+    return cmd(ssh,cf)
 
 
 class ParseOptions(object):
     def __init__(self, argv):
-        short = 'vhH:U:P:p:t:s:c:a:i:f:T:'
+        short = 'vhH:U:P:p:t:s:c:a:d:'
         long = ['help','host=','port=','username=','password=','script=','init','version','sudo','sep=',
                 'sudo-password=','connect-timeout=','append=','execute-timeout=','scripts-dir=','config=',
-                'auth-timeout=']
+                'auth-timeout=','su=','su-password=','src=','dest=']
         try:
             self.options = getopt.getopt(argv,short,long)
         except getopt.GetoptError as e:
@@ -332,22 +463,22 @@ class ParseOptions(object):
                    '    -h, --help                      Display this help and exit.\n'
                    '    -v, --version                   Display version information.\n'
                    '    -H, --host=<host>               Connect to host. [default=127.0.0.1]\n'
-                   '    -G, --group=<host>              Connect all hosts with in a group.\n'
-                   '    -i, --inventory=<filename>      Host inventory file, host or group can define in it. [default="./hosts"]\n'
                    '    -U, --username=<username>       username to use when connecting to server. [default=root]\n'
                    '    -P, --password=<password>       Password to use when connecting to server.\n'
-                   '    -f, --fork=<host>               Connect to host. [default=127.0.0.1]\n'
-                   '    -T, --threads=<host>            Connect to host. [default=127.0.0.1]\n'
                    '    -p, --port=<port>               Port number to use for connecting to server. [default=22]\n'
                    '    -t, --execute-timeout=<number>  Timeout for script execution. [default=60]\n'
-                   '    -s, --script=<script-name>      Execution this script in remote host. [vaild in script action]\n'
+                   '    -s, --src                       File source path. [vaild in put and get action]\n'
+                   '    -d, --dest                      File destination path. [vaild in put and get action]\n'
                    '    -a, --append=<arguments>        Pass arguments to the script or command of cmd.\n'
                    '    -c, --config=<path>             Use this config file. [default=/etc/remotescript.conf]\n'
                    '    \n'
                    '    --sep                           If use --append option, Use sep for arguments. [default=" "]\n'
                    '    --init                          Initialization config file and running environment.\n'
                    '    --sudo                          Run operations with sudo. [vaild in cmd and script action]\n'
-                   '    --sudo-pass                     If have --sudo option, ask for sudo password. [vaild in cmd and script action]\n'
+                   # '    --sudo-password                 If have --sudo option, ask for sudo password. [vaild in cmd and script action]\n'
+                   '    --su                            Run operations with other user. [vaild in cmd and script action]\n'
+                   '    --su-password                   If have --su option, ask for other user\'s password. [vaild in cmd and script action]\n'
+                   '    --script=<script-name>          Execution this script in remote host. [vaild in script action]\n'
                    '    --scripts-dir=<abspath>         If the script name is only the filename, it will be search in this directory. [default=/usr/share/remotescripts/]\n'
                    '    --connect-timeout=<number>      Timeout for host connect. [default=10]\n'
                    '    --auth-timeout=<number>         Timeout for user authentication. [default=10]'
@@ -357,8 +488,6 @@ class ParseOptions(object):
 
 def main():
     Config.parseConfigFile()
-    global log
-    log = Utils.logConfig(Config.config['log_file'])
     if len(sys.argv) < 3:
         print(ParseOptions.usage());sys.exit(1)
     if sys.argv[1] not in ('cmd','get','put','script'):
@@ -388,8 +517,10 @@ def main():
             Config.config['ssh_execute_timeout'] = int(value)
         if name in ('-a','--append'):
             Config.config['append'] = value
-        if name in ('-s','--script'):
-            Config.config['script'] = value
+        if name in ('-s','--src'):
+            Config.config['src'] = value
+        if name in ('-d','--dest'):
+            Config.config['dest'] = value
         if name == '--scripts-dir':
             Config.config['scripts_dir'] = value
         if name == '--sep':
@@ -402,17 +533,34 @@ def main():
             Config.config['sudo'] = True
         if name == '--sudo-password':
             Config.config['ssh_sudo_password'] = value
+        if name == '--su':
+            Config.config['su'] = value
+        if name == '--su-password':
+            Config.config['ssh_su_password'] = value
+        if name == '--script':
+            Config.config['script'] = value
+    global log
+    log = Utils.logConfig(Config.config['log_file'])
     # if not Config.config['script']:
     #     sys.stderr.write("You must privode file path of script! Use --help see more.\n")
     #     sys.stderr.flush()
     #     sys.exit(1)
     if not Config.config['ssh_password']:
-        print('Host: %s  User: %s' % (Config.config['ssh_host'],Config.config['ssh_username']))
-        Config.config['ssh_password'] = __import__('getpass').getpass("Please input password: ")
+        Config.config['ssh_password'] = __import__('getpass').getpass("Remote %s's password: "%Config.config['ssh_username'])
+    ssh = SSH(hostname=Config.config['ssh_host'], username=Config.config['ssh_username'], password=Config.config['ssh_password'],
+              timeout=Config.config['ssh_connect_timeout'],auth_timeout=Config.config['ssh_auth_timeout'])
     if sys.argv[1] == 'cmd':
-        cmd()
-
-
+        sys.exit(cmd(ssh,Config.config))
+    if sys.argv[1] == 'put':
+        src,dest = put(ssh,Config.config)
+        print("[Put]  src: %s  dest: %s successful" % (src,dest))
+        sys.exit(0)
+    if sys.argv[1] == 'get':
+        src,dest = get(ssh,Config.config)
+        print("[Get]  src: %s  dest: %s successful" % (src,dest))
+        sys.exit(0)
+    if sys.argv[1] == 'script':
+        sys.exit(script(ssh,Config.config))
 
 if __name__ == '__main__':
     try:
